b16a6600c5cda60e785a958aa5d05c7709e72f0b
Yonatan Elias <yonatane504@gmail.com>
2023-04-28 16:15:26 +0300

    nav bar changes in header component and installed react-use library

---
+++ components/Logo/index.tsx
@@ -0,0 +1,16 @@
+/* 
+
+Logo component that can be edited to fit anywhere in different configurations and qualities
+-> should be Adjustable to different parts of the websites
+-> Might be replaced by 3rd party components
+
+*/
+
+
+function index() {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
